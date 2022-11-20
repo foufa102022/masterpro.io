3a63feb0b6e83d22ecb168e786d9509fe836766a
foufa102022 <chetouiiftikhar@gmail.com>
2022-11-20 22:55:06 +0100

    version1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EtudientComponent } from './etudient/etudient.component';
+import { ProfComponent } from './prof/prof.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'ListeEtudients', component: EtudientComponent},
+  {path: 'ListeProfs', component: ProfComponent},
+  {path: '**', redirectTo: 'header'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EtudientComponent } from './etudient/etudient.component';
+import { ProfComponent } from './prof/prof.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EtudientComponent,
+    ProfComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
